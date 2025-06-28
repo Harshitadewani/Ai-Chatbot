21e45eba0cb857ededcce539159d56e5ae7a4a63
Harshita Dewani <harshitadewani@gmail.com>
2025-06-28 14:18:40 +0530

    feat: connect frontend to backend using .env

--- client/src/component/Chatbot.jsx
+++ client/src/component/Chatbot.jsx
@@ -13,7 +13,7 @@ const Chatbot = () => {
     setInput("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/chat", {
+      const res = await fetch(`${process.env.REACT_APP_API}/api/chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input }),
